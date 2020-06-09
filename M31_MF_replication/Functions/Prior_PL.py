30ca7069ec36bed101c00859a3889417b26869c3
Tobin Wainer <65673081+tobin-wainer@users.noreply.github.com>
2020-06-09 13:45:46 -0700

    Add files via upload

---
+++ M31_MF_replication/Functions/Prior_PL.py
@@ -0,0 +1,38 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def lnprior(theta):\n",
+    "    alpha = theta\n",
+    "    if -3 <= alpha <= -1 :\n",
+    "        return 0.0\n",
+    "    return -np.inf"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
