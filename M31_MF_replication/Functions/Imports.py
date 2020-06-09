35f698600215070475a0130b1fbcbe64408a2aff
Tobin Wainer <65673081+tobin-wainer@users.noreply.github.com>
2020-06-09 13:43:47 -0700

    Add files via upload

---
+++ M31_MF_replication/Functions/Imports.py
@@ -0,0 +1,62 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import numpy as np\n",
+    "from astropy.table import Table, join, MaskedColumn, vstack\n",
+    "import matplotlib.pyplot as plt\n",
+    "import matplotlib.colors as colors\n",
+    "import scipy\n",
+    "from astropy.time import Time\n",
+    "import pandas as pd\n",
+    "import re\n",
+    "import seaborn as sns\n",
+    "import datetime\n",
+    "from datetime import datetime\n",
+    "from datetime import timedelta\n",
+    "from math import e\n",
+    "from math import pi\n",
+    "from astropy.table import Column\n",
+    "from math import sqrt\n",
+    "import numpy as np\n",
+    "import emcee\n",
+    "import matplotlib.pyplot as plt\n",
+    "from astropy.io import fits\n",
+    "from astropy.table import Table\n",
+    "import math\n",
+    "import corner\n",
+    "from numpy import exp\n",
+    "from scipy import integrate\n",
+    "from scipy.integrate import quad\n",
+    "from scipy.integrate import nquad\n",
+    "import pdb\n",
+    "import powerlaw"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
